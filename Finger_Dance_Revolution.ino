f8a7c79a1cf9fa2dd975a4e5110fb12a2e359c1f
Harry Zhang <zh.harry@yahoo.ca>
2017-01-10 21:10:37 -0500

    Game on Button 8

--- Finger_Dance_Revolution.ino
+++ Finger_Dance_Revolution.ino
@@ -1,9 +1,100 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <CountUpDownTimer.h>
+CountUpDownTimer T(UP, HIGH); 
 
+int status = 13;
+
+// False = Low, True = High
+bool last[10];
+// Where fps means how many samples to take per second and 
+// time is how many samples have elapsed
+int fps = 250; 
+
+// Game Constants
+const int doubPoints = 25;
+const int fullPoints = 100;
+const int halfPoints = 250;
+const int noPoints = 500; 
+const int ppc = 100; // Points Per Click
+
+// Songs
+int testSong[] = {2500, 5000, 7500, 10000, 12500, 15000, 17500};
+int nextNote = 0;
+int score = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(status, OUTPUT);
+  Serial.println("------");
+    
+  for (int i = 2; i <= 9; i++)
+    pinMode(i, INPUT);
+    
+  T.SetStopTime(7200);
+  T.StartTimer();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(status, HIGH);
+  T.Timer(); // run the timer
+    
+  if (nextNote >= (sizeof(testSong) / sizeof(int))) {
+    Serial.println("SO"); // Song Over
+    Serial.print("Final Score: ");
+    Serial.println(score, DEC);
+    while (true)
+      delay(1000);
 
+  } else { 
+    int curTime = T.ShowMilliSeconds();
+
+    // If the player missed the current note
+    if (testSong[nextNote] + noPoints < curTime) {
+      Serial.print("MN:"); // Missed Note
+      Serial.print(nextNote, DEC);
+      Serial.print(" T:"); // Expected Time
+      Serial.print(testSong[nextNote], DEC);        
+      Serial.print(" @ "); // Current Time
+      Serial.println(curTime, DEC);
+      nextNote++;
+    }
+    
+    if (digitalRead(8) == HIGH) {
+      if (!last[8]) {
+        last[8] = true;
+        int delta = -(testSong[nextNote] - curTime);
+        int deltaVal = abs(delta);
+        
+        Serial.print("CN:"); // Current Note
+        Serial.print(nextNote, DEC);
+        Serial.print(" T:"); // Expected Time
+        Serial.print(testSong[nextNote], DEC);        
+        Serial.print(" @ "); // Current Time
+        Serial.print(curTime, DEC);
+        Serial.print(delta >= 0 ? " +" : " ");
+        Serial.print(delta, DEC); // Time Difference
+        
+        nextNote++;
+        if (deltaVal <= doubPoints) {
+          Serial.print(" DPA "); // Double Points Awarded
+          score += ppc * 2;
+        } else if (deltaVal <= fullPoints) {
+          Serial.print(" FPA "); // Full Points Awarded
+          score += ppc;
+        } else if (deltaVal <= halfPoints) {
+          Serial.print(" HPA "); // Half Points Awarded
+          score += ppc / 2;
+        } else if (deltaVal <= noPoints) {
+          Serial.print(" NPA "); // No Points (Too Early/Late) Awarded
+        } else {
+          Serial.print(" NAY "); // Not avaliable yet
+          nextNote--;
+        }
+      
+        Serial.println();
+      }
+    } else
+      last[8] = false; 
+  }
+  
+  delay(1000 / fps);
 }
