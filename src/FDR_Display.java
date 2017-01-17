4a78f85c4c662bafa30216f687e7347caea172e8
Harry Zhang <zh.harry@yahoo.ca>
2017-01-17 17:53:07 -0500

    sdhg

--- src/FDR_Display.java
+++ src/FDR_Display.java
@@ -5,6 +5,7 @@ import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import javax.imageio.ImageIO;
@@ -33,7 +34,7 @@ public class FDR_Display {
 	static BufferedImage sprLeftB, sprRightB, sprTopB, sprDownB;
 
 	static File serial;
-	static BufferedReader s;
+	static InputStream s;
 
 	public static void main(String[] args) throws Exception {
 
@@ -48,7 +49,7 @@ public class FDR_Display {
 		sprDownB = ImageIO.read(new File("DownBlank.png"));
 
 		serial = new File("/dev/ttyAMA0");
-		s = new BufferedReader(new InputStreamReader(new FileInputStream(serial)));
+		s = new FileInputStream(serial);
 
 		// Start Game
 		running = true;
@@ -57,17 +58,20 @@ public class FDR_Display {
 		// Game Loop
 		new Thread() {
 			public void run() {
-				while (true) {
+				//while (true) {
 					try {
 						//if (processed) {
 						//	commands = "";
 						//	processed = false;
 						//}
-						commands = s.readLine();
-						System.out.println(commands);
+						int c;
+						while((c = s.read()) != -1) {
+						    System.out.print((char)c);
+						}
+						//System.out.println(commands);
 					} catch (Exception e) {
 					}
-				}
+				//}
 			}
 		}.start();
 		while (running) {
