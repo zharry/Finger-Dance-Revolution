8582771b33794ed602bd0799ef16972130c1b694
Harry Zhang <zh.harry@yahoo.ca>
2017-01-17 22:42:36 -0500

    sdgsdg

--- src/FDR_Display.java
+++ src/FDR_Display.java
@@ -107,7 +107,7 @@ public class FDR_Display {
 							} else if (c.equals("SS")) {
 								gameOver = false;
 								Runtime.getRuntime().exec("pkill omxplayer");
-								ProcessBuilder pb = new ProcessBuilder("bash", "-c", "omxplayer -r -o hdmi myVideoFile.mp4");
+								ProcessBuilder pb = new ProcessBuilder("bash", "-c", "omxplayer -o local /home/pi/Finger-Dance-Revolution/Song.wav");
 								Process process = pb.start();
 							}
 						}
